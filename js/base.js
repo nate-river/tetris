cb17d2105907b82231920dba392d0f8354b9c8f5
Ma Yanlong <l.luffy.river@gmail.com>
2015-05-11 09:02:28 +0800

    base

--- js/base.js
+++ js/base.js
@@ -1,7 +1,5 @@
 window.onload = function(){
     var canvas = document.getElementById('canvas'),
-	canvasWidth = 200,
-	canvasHeight = 300,
 	_ctx = canvas.getContext('2d');
     _ctx.fillStyle = '#d6ffb2';
 
@@ -13,6 +11,22 @@ window.onload = function(){
 	    t.zuoyou ++;
 	}
     }
+    //画出主场景，致敬第一款俄罗斯方块的界面
+    function drawSence (){
+	for ( var i = 0;  i < 16;  i++){
+	    for ( var j=0; j<10; j++){
+		var x = 20*j + 9;
+		var y = 21*i + 10; 
+		_ctx.fillRect(x,y,2,2);
+	    }
+	}
+    };
+    drawSence();
+    //用来根据坐标在画布中画出一个块
+    function drawBlock (i,j){
+	_ctx.fillRect(i*20+2,j*21+3,18,18);
+    }
+
     function tetris (){
 	this.maps = [
 	    [
@@ -35,70 +49,117 @@ window.onload = function(){
 	this.zuoyou = 3;
 	this.remain = [ [0,15],[1,15],[2,15],[3,15],[4,15] ];
     }
-
+    //随机选取一种形状出现
     tetris.prototype.born = function(){
 	this.curentShape = this.maps[ Math.round( Math.random()* (this.maps.length-1) ) ];
     }
+
+    //画出累积页面上的块
     tetris.prototype.drawRemain = function(){
 	for ( var i = 0;  i < this.remain.length;  i++){
 	    drawBlock(this.remain[i][0],this.remain[i][1]);
 	}
     }
+
+    //根据随机出现的形状，画出形状中的各个块
     tetris.prototype.drawShape = function(){
-	var tmp = {isPeng:false,tmp:[]};
+	var tmp = []; 
 	for ( var i = 0;  i < this.curentShape.length;  i++){
 	    for ( var j=0; j< this.curentShape[i].length; j++){
 		if(this.curentShape[i][j]){
 		    var x = j+this.zuoyou,
 			y = i+this.xia;
-		    //画块的时候如果画到了已有的块上，证明碰撞了。返回false;
-		    var that  = this;
-		    var isPeng = function(){
-			for ( var i = 0;  i < that.remain.length;  i++){
-			    if( x == that.remain[i][0] && y == that.remain[i][1]){
-				return true;
-			    }
-			}
-			return false;
-		    }();
 		    drawBlock( x, y);
-		    tmp['isPeng'] = isPeng;
-		    tmp['tmp'].push([j+this.zuoyou,i+this.xia]);
+		    tmp.push([x,y]);
 		}
 	    }
 	}
 	return tmp;
     }
+    //画形状中的各个块之前，先判断一下
+    tetris.prototype.dapanduan = function(){
+	if(this.xia > 13){
+	    return false;
+	}
+	for ( var i = 0;  i < this.curentShape.length;  i++){
+	    for ( var j=0; j<this.curentShape[i].length; j++){
+		if( this.curentShape[i][j] ){
+		    var x = j + this.zuoyou,
+			y = i + this.xia;
+		    if( this.pandan(x,y) ){
+			return false;
+		    }
+		}
+		    
+	    }
+	}
+	return true;
+    }
+    //传入坐标x,y,判断页面中该位置上是否有已画好的块
+    tetris.prototype.pandan =function(x,y){
+	for ( var i = 0;  i < this.remain.length;  i++){
+	    if( x == this.remain[i][0] && y == this.remain[i][1] ){
+		return true;
+	    }
+	}
+	return false;
+    }
+    tetris.prototype.start = function(){
+	var that = this;
+	var tmp;
+	this.interId = setInterval(function(){
+	    _ctx.clearRect(0,0,202,339);
+	    drawSence();
+	    that.drawRemain();
+
+    	    if( that.dapanduan() ){
+		tmp = that.drawShape();
+    		that.xia ++;
+    	    }else{
+		that.remain = that.remain.concat(tmp);
+		for ( var i = 0;  i < tmp.length;  i++){
+		    drawBlock(tmp[i][0],tmp[i][1]);
+		}
+		//这里需要看看remain里面有没有一行已经满了的情况。做消除处理
+		that.xiaochu(tmp);
+
+    		that.xia = 0;
+    		that.zuoyou = 3;
+    		that.born();
+	    }
+	},200)
+    }
+    tetris.prototype.xiaochu = function(tmp){
+	// this.remain = [ [0,15],[1,15],[2,15],[3,15],[4,15] ];
+	// tmp = [ [4,13],[4,14],[5,14],[5,15] ];
+
+	//根据tmp的y坐标来判断。看看this remain里这个y坐标下是否有10个元素，如果有，代表这一行满，把他们全消除掉
+	var aa = [];
+	for ( var i = 0;  i < tmp.length;  i++){
+	    aa.push(tmp[i][1]);
+	}
+
+	while( aa.length ){
+	    var y = aa.pop();
+	    var count = 0,
+		t = [];
+	    for ( var i = 0;  i < this.remain.length;  i++){
+		if ( this.remain[i][1] == y ){
+		    count++;  
+		    continue;
+		}
+		t.push(this.remain[i]);
+ 	    }
+	    if(count == 10){
+		console.log('man');
+		this.remain = t;
+	    }
+	};
+    }
 
     var t = new tetris();
     t.born();
+    t.drawShape();
+    // t.start();
     
-    var iterId = setInterval(function(){
-    	_ctx.clearRect(0,0,202,339);
-    	drawSence();
-    	t.drawRemain();
-    	var lastpos = t.drawShape();
-    	t.xia ++;
-    	if( t.xia > 13 || lastpos.isPeng ){
-    	    //重新出块之前把上次那个块停下的位置送人this.remain;
-    	    t.remain = t.remain.concat(lastpos.tmp);
-    	    t.xia = 0;
-    	    t.zuoyou = 3;
-    	    t.born();
-    	}
-    },200);
-
-    function drawSence (){
-	for ( var i = 0;  i < 16;  i++){
-	    for ( var j=0; j<10; j++){
-		var x = 20*j + 9;
-		var y = 21*i + 10; 
-		_ctx.fillRect(x,y,2,2);
-	    }
-	}
-    };
-    drawSence();
-    function drawBlock (i,j){
-	_ctx.fillRect(i*20+2,j*21+3,18,18);
-    }
 }
