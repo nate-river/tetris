ab82db4c0a42f82c76ff3f20d1594bdf5a92cf4f
Ma Yanlong <l.luffy.river@gmail.com>
2015-07-12 22:14:29 +0800

    ..

--- js/base.js
+++ js/base.js
@@ -1,78 +1,87 @@
 window.onload = function(){
-    var canvas = document.getElementById('canvas'),
-	_ctx = canvas.getContext('2d');
-    _ctx.fillStyle = '#d6ffb2';
-
-    document.onkeydown = function(e){
-	if(e.keyCode == 37){
-	    t.zuoyou --;
-	}
-	if(e.keyCode == 39){
-	    t.zuoyou ++;
-	}
-	if(e.keyCode == 38){
-	    t.bianxing();
-	}
-	if(e.keyCode == 40){
-	    t.xia++;
-	}
-	_ctx.clearRect(0,0,202,339);
-	drawSence();
-	t.drawShape();
-	t.drawRemain();
-	e.preventDefault();
-    }
-    //画出主场景，致敬第一款俄罗斯方块的界面
-    function drawSence (){
-	for ( var i = 0;  i < 16;  i++){
-	    for ( var j=0; j<10; j++){
-		var x = 20*j + 9;
-		var y = 21*i + 10; 
-		_ctx.fillRect(x,y,2,2);
-	    }
-	}
-    };
-    drawSence();
-    //用来根据坐标在画布中画出一个块
-    function drawBlock (i,j){
-	_ctx.fillRect(i*20+2,j*21+3,18,18);
-    }
-
+    //主对象
     function tetris (){
-	this.maps = [
-	    [
-		[0,1,0],
-		[1,1,1],
-		[0,0,0],
-	    ],
-	    [
-		[1,1],
-		[1,1]
-	    ],
-	    [
-		[0,1,0],
-		[0,1,1],
-		[0,0,1],
-	    ],
-	    [
-		[0,0,1,0],
-		[0,0,1,0],
-		[0,0,1,0],
-		[0,0,1,0]
-	    ],
-	    [
-		[0,1,0],
-		[0,1,0],
-		[0,1,1],
-	    ],
-	];
+	var canvas = document.getElementById('canvas');
+	this._ctx = canvas.getContext('2d');
+	this._ctx.fillStyle = '#d6ffb2';
 	this.xia = 0;
 	this.zuoyou = 3;
 	this.remain = [];
+	this.eventsHandle();
+	this.maps = [[[0,1,0], [1,1,1], [0,0,0],], [[1,1], [1,1]], [[0,1,0], [0,1,1], [0,0,1],], [[0,1,0], [1,1,0], [1,0,0],], [[0,1,0,0], [0,1,0,0], [0,1,0,0], [0,1,0,0]], [[0,1,0], [0,1,0], [0,1,1],], [[0,1,1], [0,1,0], [0,1,0],]];
     }
 
+    //画主场景，致敬第一款俄罗斯方块的界面
+    tetris.prototype.drawSence = function(){
+	for ( var i = 0;  i < 16;  i++){
+	    for ( var j=0; j<10; j++){
+		var x = 20*j + 9;
+		var y = 21*i + 10;
+		this._ctx.fillRect(x,y,2,2);
+	    }
+	}
+    };
+    //根据坐标在画布中画出一个块
+    tetris.prototype.drawBlock =function(i,j){
+	this._ctx.fillRect(i*20+2,j*21+3,18,18);
+    };
+
+    //画出页面上未消除的块
+    tetris.prototype.drawRemain = function(){
+	for ( var i = 0;  i < this.remain.length;  i++){
+	    this.drawBlock(this.remain[i][0],this.remain[i][1]);
+	}
+    };
+
+    //随机选取一种形状
+    tetris.prototype.born = function(){
+	this.curentShape = this.maps[ Math.round( Math.random()* (this.maps.length-1) ) ];
+    };
+
+    //画形状中的各个块之前，先判断一下
+    tetris.prototype.dapanduan = function(){
+	for ( var i = 0;  i < this.curentShape.length;  i++){
+	    for ( var j=0; j<this.curentShape[i].length; j++){
+		if( this.curentShape[i][j] ){
+		    var x = j + this.zuoyou,
+			y = i + this.xia;
+		    console.log(x,y);
+		    if( this.pandan(x,y) || y > 15 ){
+			return false;
+		    }
+		}
+	    }
+	}
+	return true;
+    };
+    //传入坐标x,y,判断页面中该位置上是否有已画好的块
+    tetris.prototype.pandan =function(x,y){
+	for ( var i = 0;  i < this.remain.length;  i++){
+	    if( x == this.remain[i][0] && y == this.remain[i][1] ){
+		return true;
+	    }
+	}
+	return false;
+    };
+
+    //根据随机出现的形状，画出形状中的各个块
+    tetris.prototype.drawShape = function(){
+	var tmp = [];
+	for ( var i = 0;  i < this.curentShape.length;  i++){
+	    for ( var j=0; j< this.curentShape[i].length; j++){
+		if(this.curentShape[i][j]){
+		    var x = j+this.zuoyou,
+			y = i+this.xia;
+		    this.drawBlock( x, y);
+		    tmp.push([x,y]);
+		}
+	    }
+	}
+	return tmp;
+    };
+
+    //(二维数组顺时针旋转90度)块变形的数据处理
     tetris.prototype.bianxing = function(){
-	//二维数组顺时针旋转90度
 	var matrix = this.curentShape;
 	var n = matrix.length;
         var limit = (n-1)/2;
@@ -85,13 +94,36 @@ window.onload = function(){
                 matrix[j][n-1-i] = temp;
             }
         }
-	_ctx.clearRect(0,0,202,339);
-	drawSence();
+	this._ctx.clearRect(0,0,202,339);
+	this.drawSence();
 	this.drawShape();
-    }
+    };
 
+    //处理操控事件
+    tetris.prototype.eventsHandle = function(){
+	var that = this;
+	document.onkeydown = function(e){
+	    if(e.keyCode == 37){
+		that.zuoyou --;
+	    }
+	    if(e.keyCode == 39){
+		that.zuoyou ++;
+	    }
+	    if(e.keyCode == 38){
+		that.bianxing();
+	    }
+	    if(e.keyCode == 40){
+		that.xia++;
+	    }
+	    that._ctx.clearRect(0,0,202,339);
+	    that.drawSence();
+	    that.drawRemain();
+	    that.drawShape();
+	    e.preventDefault();
+	};
+    };
 
-    //以下三个用来处理块的消除
+    //处理块的消除
     tetris.prototype.positionToData = function(){
 	this.remainData = {};
 	for ( var i = 0;  i < this.remain.length;  i++){
@@ -100,7 +132,7 @@ window.onload = function(){
 	for ( var i = 0;  i < this.remain.length;  i++){
 	    this.remainData[ this.remain[i][1] ].push(this.remain[i][0]);
 	}
-    }
+    };
 
     tetris.prototype.xiaochu = function(){
 	var t = [];
@@ -113,12 +145,12 @@ window.onload = function(){
 	var tmp = {};
 	if( t.length ){
 	    for ( var i = 15;  i > ( 15 - t.length);  i--){
-		tmp[i] = t[15-i]
+	      tmp[i] = t[15-i];
 	    }
 	}
 	this.remainData = tmp;
 	this.dataToPosition();
-    }
+    };
 
     tetris.prototype.dataToPosition = function(){
 	this.remain  = [];
@@ -127,76 +159,24 @@ window.onload = function(){
 		this.remain.push( [this.remainData[i][j],window.parseInt(i)] );
 	    }
 	}
-    }
+    };
 
-    //随机选取一种形状出现
-    tetris.prototype.born = function(){
-	this.curentShape = this.maps[ Math.round( Math.random()* (this.maps.length-1) ) ];
-    }
-
-    //画出累积页面上的块
-    tetris.prototype.drawRemain = function(){
-	for ( var i = 0;  i < this.remain.length;  i++){
-	    drawBlock(this.remain[i][0],this.remain[i][1]);
-	}
-    }
-
-    //根据随机出现的形状，画出形状中的各个块
-    tetris.prototype.drawShape = function(){
-	var tmp = []; 
-	for ( var i = 0;  i < this.curentShape.length;  i++){
-	    for ( var j=0; j< this.curentShape[i].length; j++){
-		if(this.curentShape[i][j]){
-		    var x = j+this.zuoyou,
-			y = i+this.xia;
-		    drawBlock( x, y);
-		    tmp.push([x,y]);
-		}
-	    }
-	}
-	return tmp;
-    }
-    //画形状中的各个块之前，先判断一下
-    tetris.prototype.dapanduan = function(){
-	for ( var i = 0;  i < this.curentShape.length;  i++){
-	    for ( var j=0; j<this.curentShape[i].length; j++){
-		if( this.curentShape[i][j] ){
-		    var x = j + this.zuoyou,
-			y = i + this.xia;
-		    if( this.pandan(x,y) || y > 15 ){
-			return false;
-		    }
-		}
-		    
-	    }
-	}
-	return true;
-    }
-    //传入坐标x,y,判断页面中该位置上是否有已画好的块
-    tetris.prototype.pandan =function(x,y){
-	for ( var i = 0;  i < this.remain.length;  i++){
-	    if( x == this.remain[i][0] && y == this.remain[i][1] ){
-		return true;
-	    }
-	}
-	return false;
-    }
-
+    //开始和暂停
     tetris.prototype.start = function(){
+	this.born();
 	var that = this;
-	var tmp;
 	this.interId = setInterval(function(){
-	    _ctx.clearRect(0,0,202,339);
-	    drawSence();
+	    that._ctx.clearRect(0,0,202,339);
+	    that.drawSence();
 	    that.drawRemain();
 
     	    if( that.dapanduan() ){
-		tmp = that.drawShape();
+		that.tmp = that.drawShape();
     		that.xia ++;
     	    }else{
-		that.remain = that.remain.concat(tmp);
-		for ( var i = 0;  i < tmp.length;  i++){
-		    drawBlock(tmp[i][0],tmp[i][1]);
+		that.remain = that.remain.concat(that.tmp);
+		for ( var i = 0;  i < that.tmp.length;  i++){
+		    that.drawBlock(that.tmp[i][0],that.tmp[i][1]);
 		}
 		//消除
 		that.positionToData();
@@ -211,11 +191,12 @@ window.onload = function(){
     		that.zuoyou = 3;
     		that.born();
 	    }
-	},200)
-    }
+	},250);
+    };
+    tetris.prototype.pause = function(){
+	clearInterval(this.interId);
+    };
 
     var t = new tetris();
-    t.born();
-    t.drawShape();
     t.start();
-}
+};
